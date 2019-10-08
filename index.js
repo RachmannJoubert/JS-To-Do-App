40af0b7b3be216ac9b59ac6aeec21e3498dabe83
Rachmann <rachmann.joubert@gmail.com>
2019-10-08 16:57:33 +0100

    Added function to get tasks and route to display them

--- index.js
+++ index.js
@@ -7,3 +7,24 @@ const mongoClient = require('mongodb').MongoClient
 const url = 'mongodb://localhost:27017'
 
 let jsonParser = bodyParser.json()
+
+let getTasks = function (db, callback) {
+    let collection = db.collection('Tasks')
+
+    collection.find({}).toArray(function (err, docs) {
+        console.log('All current tasks')
+        callback(docs)
+    })
+}
+
+app.get('/tasks', function (req, res) {
+    mongoClient.connect(url, {useNewUrlParser: true, useUnifiedTopology: true}, function (err, client) {
+        console.log('Successfully connected')
+
+        let db = client.db('ToDoLis')
+        getTasks(db, function (result) {
+            console.log(result)
+            res.json(result)
+        })
+    })
+})
